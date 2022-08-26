30fad7096f4d333b27bb5f89309f058b6340bc2b
Kaushik Kakdey <kaushik.kakdey@gmail.com>
2022-08-26 19:34:15 +0530

    Users module feature

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+
+class User(AbstractUser):
+
+    username = models.CharField(max_length=20, unique=True, blank=False, null=False)
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.username}"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from users.models import User
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
+
+
+@api_view(["POST"])
+def register(request):
+    try:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if User.objects.filter(username=username).exists():
+            raise Exception("User alreaddy exists!")
+
+        new_user = User(username=username, password=password)
+        new_user.save()
+
+        token = Token.objects.create(user=new_user)
+        return Response({"token": token.key})
+
+    except Exception as e:
+        print(str(e))
+        raise e
+
+
+
+
+
+@api_view(["POST"])
+def login(request):
+    try:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = User.objects.get(username=username, password=password)
+
+        token = Token.objects.get(user=user)
+        return Response({"token": token.key})    
+
+    except Exception as e:
+        print(str(e))
+        raise e
